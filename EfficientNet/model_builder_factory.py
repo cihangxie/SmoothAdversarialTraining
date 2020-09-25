bf651538e5fa9c22198a0930e835f9e1a12ac01c
Mingxing Tan <tanmingxing@google.com>
2020-09-25 15:06:52 -0700

    Clean up efficientnet.

--- EfficientNet/model_builder_factory.py
+++ EfficientNet/model_builder_factory.py
@@ -15,49 +15,15 @@
 """Utilities for model builder or input size."""
 
 import efficientnet_builder
-from condconv import efficientnet_condconv_builder
-from edgetpu import efficientnet_edgetpu_builder
-from lite import efficientnet_lite_builder
-from tpu import efficientnet_x_builder
 
 
 def get_model_builder(model_name):
   """Get the model_builder module for a given model name."""
-  if model_name.startswith('efficientnet-lite'):
-    return efficientnet_lite_builder
-  elif model_name.startswith('efficientnet-edgetpu-'):
-    return efficientnet_edgetpu_builder
-  elif model_name.startswith('efficientnet-condconv-'):
-    return efficientnet_condconv_builder
-  elif model_name.startswith('efficientnet-x-'):
-    return efficientnet_x_builder
-  elif model_name.startswith('efficientnet-'):
-    return efficientnet_builder
-  else:
-    raise ValueError(
-        'Model must be either efficientnet-b* or efficientnet-edgetpu* or'
-        'efficientnet-condconv*, efficientnet-lite*')
+  return efficientnet_builder
 
 
 def get_model_input_size(model_name):
   """Get model input size for a given model name."""
-  if model_name.startswith('efficientnet-lite'):
-    _, _, image_size, _ = (
-        efficientnet_lite_builder.efficientnet_lite_params(model_name))
-  elif model_name.startswith('efficientnet-edgetpu-'):
-    _, _, image_size, _ = (
-        efficientnet_edgetpu_builder.efficientnet_edgetpu_params(model_name))
-  elif model_name.startswith('efficientnet-condconv-'):
-    _, _, image_size, _, _ = (
-        efficientnet_condconv_builder.efficientnet_condconv_params(model_name))
-  elif model_name.startswith('efficientnet-x'):
-    _, _, image_size, _, _ = efficientnet_x_builder.efficientnet_x_params(
-        model_name)
-  elif model_name.startswith('efficientnet'):
-    _, _, image_size, _ = efficientnet_builder.efficientnet_params(model_name)
-  else:
-    raise ValueError(
-        'Model must be either efficientnet-b* or efficientnet-x-b* or efficientnet-edgetpu* or '
-        'efficientnet-condconv*, efficientnet-lite*')
+  _, _, image_size, _ = efficientnet_builder.efficientnet_params(model_name)
   return image_size
 
